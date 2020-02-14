85fec85be4d5fb54e5307d6c00f218d97cd8bc12
kshah3 <kcshah@ncsu.edu>
2020-02-14 14:40:45 -0500

    Setup template in repo.

---
+++ commands/setup.js
@@ -0,0 +1,55 @@
+const child = require('child_process');
+const chalk = require('chalk');
+const path = require('path');
+const os = require('os');
+
+const scpSync = require('../lib/scp');
+const sshSync = require('../lib/ssh');
+
+exports.command = 'setup';
+exports.desc = 'Provision and configure the configuration server';
+exports.builder = yargs => {
+    yargs.options({
+        privateKey: {
+            describe: 'Install the provided private key on the configuration server',
+            type: 'string'
+        }
+    });
+};
+
+
+exports.handler = async argv => {
+    const { privateKey } = argv;
+
+    (async () => {
+
+        await run( privateKey );
+
+    })();
+
+};
+
+async function run(privateKey) {
+
+    console.log(chalk.greenBright('Installing configuration server!'));
+
+    console.log(chalk.blueBright('Provisioning configuration server...'));
+    let result = child.spawnSync(`bakerx`, `run ansible-srv bionic --ip 192.168.33.11 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Provisioning mattermost server...'));
+    result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip 192.168.33.81`.split(' '), {shell:true, stdio: 'inherit'} );
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Installing privateKey on configuration server'));
+    let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
+    result = scpSync (identifyFile, 'vagrant@192.168.33.11:/home/vagrant/.ssh/mm_rsa');
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Running init script...'));
+    result = sshSync('/bakerx/cm/server-init.sh', 'vagrant@192.168.33.11');
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+
+
+}
