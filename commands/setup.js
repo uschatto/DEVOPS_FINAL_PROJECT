85e9070b6ee92cc681767281d8cfd9900a6c1683
Udita Chattopadhyay <uschatto@ncsu.edu>
2020-02-14 19:23:43 -0500

    Add code to setup.js to run the ansible playbook with the command pipeline setup

--- commands/setup.js
+++ commands/setup.js
@@ -13,23 +13,40 @@ exports.builder = yargs => {
         privateKey: {
             describe: 'Install the provided private key on the configuration server',
             type: 'string'
+        },
+        file: {
+            describe: 'Provide the path to the main playbook.yml',
+            type: 'string',
+            default: 'pipeline/playbook.yml'
+        },
+        inventory: {
+            describe: 'Provide the path to the inventory file',
+            type: 'string',
+            default: 'pipeline/inventory.ini'
         }
     });
 };
 
 
 exports.handler = async argv => {
-    const { privateKey } = argv;
+    const { privateKey, file, inventory } = argv;
 
     (async () => {
 
-        await run( privateKey );
+        if (fs.existsSync(path.resolve(file)) && fs.existsSync(path.resolve(inventory))) 
+        {
+             await run( privateKey, file, inventory );
+        }
+        else
+        {
+             console.error(`File or inventory don't exist. Make sure to provide path from root of pipeline directory`);  
+        }          
 
     })();
 
 };
 
-async function run(privateKey) {
+async function run(privateKey, file, inventory) {
 
     console.log(chalk.greenBright('Installing configuration server!'));
 
@@ -50,4 +67,12 @@ async function run(privateKey) {
     result = sshSync('/bakerx/pipeline/server-init.sh', 'vagrant@192.168.33.11');
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
+    // the paths should be from root of pipeline directory
+    // Transforming path of the files in host to the path in VM's shared folder
+    let filePath = '/bakerx/'+ file;
+    let inventoryPath = '/bakerx/' +inventory;
+
+    console.log(chalk.blueBright('Running ansible script...'));
+    result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath}`, 'vagrant@192.168.33.10');
+    if( result.error ) { process.exit( result.status ); }
 }
