b3c1b80d452fd7bc14ee45a0c3b201ae0597fa5a
kshah3 <kcshah@ncsu.edu>
2020-02-17 18:19:48 -0500

    Turning off jenkins setup wizard through automation script.

--- pipeline/roles/all/templates/jenkins_script.groovy
+++ pipeline/roles/all/templates/jenkins_script.groovy
@@ -7,12 +7,15 @@ import jenkins.install.*;
 
 
 def instance = Jenkins.getInstance()
+println "--> creating local user 'admin'"
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 
 hudsonRealm.createAccount("{{jenkins_user}}","{{jenkins_password}}")
 instance.setSecurityRealm(hudsonRealm)
 
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 
 instance.save()
