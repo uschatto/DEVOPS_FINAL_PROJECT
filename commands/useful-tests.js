6dd77d972bfbc6be45c6e0cab7524bd009531eec
Udita Chattopadhyay <uschatto@ncsu.edu>
2020-03-25 04:25:35 -0400

    Initial commit for fuzzing, implemented the logic for extracting 10% random files

---
+++ commands/useful-tests.js
@@ -0,0 +1,69 @@
+const child = require('child_process');
+const chalk = require('chalk');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+require('dotenv').config();
+
+const scpSync = require('../lib/scp');
+const sshSync = require('../lib/ssh');
+
+exports.command = 'useful-tests';
+exports.desc = 'Initiate analysis of test suite for iTrust to run `-c` numbers of times';
+exports.builder = yargs => {
+    yargs.options({
+        file: {
+            describe: 'Provide the path to the main playbook.yml',
+            type: 'string',
+            default: 'pipeline/fuzzer.yml'
+        },
+        inventory: {
+            describe: 'Provide the path to the inventory file',
+            type: 'string',
+            default: 'pipeline/inventory.ini'
+        },
+        vaultpass: {
+            alias: 'vp',
+            describe: 'the password to use for ansible vault',
+            default: 'DEVOPS16',
+            type: 'string'
+        },
+	count: {
+            alias: 'c',
+	    describe: 'test suite for iTrust to run `-c` numbers of times.',
+            type: 'int'
+        }
+    });
+};
+
+
+exports.handler = async argv => {
+    const { file, inventory, vaultpass, count } = argv;
+    (async () => {
+
+        if (fs.existsSync(path.resolve(file)) && fs.existsSync(path.resolve(inventory))) 
+        {
+             await run( file, inventory, vaultpass, count );
+        }
+        else
+        {
+             console.error(`'File or inventory dont exist. Make sure to provide path from root of pipeline directory'`);  
+        }          
+
+    })();
+
+};
+
+async function run(file, inventory, vaultpass, count) {
+
+    // the paths should be from root of pipeline directory
+    // Transforming path of the files in host to the path in VM's shared folder
+    let filePath = '/bakerx/'+ file;
+    let inventoryPath = '/bakerx/' +inventory;
+
+    let vaultPath = '/tmp/.vault_pass';
+
+    console.log(chalk.blueBright('Running ansible script...'));
+    result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath} ${vaultPath} ${count}`, 'vagrant@192.168.33.11');
+    if( result.error ) { process.exit( result.status ); }
+}
---
+++ pipeline/roles/itrust_fuzzer/files/fuzzer.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+const getFilesRecursively = (dir, filelist = [])=> {
+         fs.readdirSync(dir).forEach(file => {
+                filelist = fs.statSync(path.join(dir,file)).isDirectory() ?
+                                               getFilesRecursively(path.join(dir,file),filelist) : filelist.concat(path.join(dir,file))
+         })
+        return filelist
+}
+
+const fuzzer = (filepath)=> {
+        //Implement fuzzing logic on the filepath
+        console.log(filepath)
+}
+
+const runFuzzer= (n) =>
+{
+        //Get the list of filenames in an array
+        let javaPaths = getFilesRecursively('src/main/java/edu/ncsu/csc/itrust2')
+        let arrayLength = javaPaths.length
+        let indexAlreadyFuzzed = []
+
+        //Store the 10% of the total array length in a variable
+        let count = Math.floor(0.1 * arrayLength)
+
+        //Randomly select files to be acted upon and check if reached the count
+        while (count != 0){
+                let rand = Math.floor(Math.random() * arrayLength)
+                if (!indexAlreadyFuzzed.includes(rand))
+                {
+                        indexAlreadyFuzzed.push(rand)
+                        count = count - 1
+                        fuzzer(javaPaths[rand])
+                }
+        }
+}
+
+runFuzzer(3)
