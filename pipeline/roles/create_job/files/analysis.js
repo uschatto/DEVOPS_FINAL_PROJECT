c6ef31072c5549eac06634024edcfd2b8b171db3
Udita Chattopadhyay <uschatto@ncsu.edu>
2020-03-29 01:05:57 -0400

    Fixing few indentation errors

--- pipeline/roles/create_job/files/analysis.js
+++ pipeline/roles/create_job/files/analysis.js
@@ -36,13 +36,13 @@ var builders = {};
 
 function FunctionBuilder()
 {
-	// The max depth of scopes (nested ifs, loops, etc)
+        // The max depth of scopes (nested ifs, loops, etc)
         this.MaxNestingDepth    = 0;
-	// The max length of a message chain in a function.
-	this.MaxMessageChain = 0; 
-	// The function name will be reported here.
+        // The max length of a message chain in a function.
+        this.MaxMessageChain = 0; 
+        // The function name will be reported here.
         this.FunctionName = "";
-	this.report = function()
+        this.report = function()
         {
 		console.log("\tFunctionName : " + this.FunctionName + " MaxNestingDepth : " + this.MaxNestingDepth + " MaxMessageChain : " + this.MaxMessageChain);
         }
@@ -52,7 +52,8 @@ function FunctionBuilder()
 function FileBuilder()
 {
 	this.FileName = "";
-                                                                                                                                                                            this.report = function()
+
+        this.report = function()
         {
 		console.log("Filename : " + this.FileName);
         }
@@ -61,27 +62,33 @@ function FileBuilder()
 // A function following the Visitor pattern.
 // Annotates nodes with parent objects.
 function traverseWithParents(object, visitor)
-{                                                                                                                                                                       var key, child;
-                                                                                                                                                                        visitor.call(null, object);
-                                                                                                                                                                        for (key in object) {                                                                                                                                                   if (object.hasOwnProperty(key)) {
-            child = object[key];                                                                                                                                                if (typeof child === 'object' && child !== null && key != 'parent')                                                                                                 {                                                                                                                                                                       child.parent = object;                                                                                                                                              traverseWithParents(child, visitor);
+{
+    var key, child;
+    visitor.call(null, object);
+
+    for (key in object) {
+        if (object.hasOwnProperty(key)) {
+            child = object[key];
+            if (typeof child === 'object' && child !== null && key != 'parent')
+            {
+		child.parent = object;
+		traverseWithParents(child, visitor);
             }
         }
     }
 }
 
 
-
 function complexity(filePath)
 {
-	var buf = fs.readFileSync(filePath, "utf8");
+        var buf = fs.readFileSync(filePath, "utf8");
         var ast = esprima.parse(buf, options);
         var i = 0;
 
-	var fileBuilder = new FileBuilder();
+        var fileBuilder = new FileBuilder();
         fileBuilder.FileName = path.basename(filePath);
 	
-	traverseWithParents(ast, function (node)
+        traverseWithParents(ast, function (node)
         {
                 if (node.type === 'FunctionDeclaration')
                 {
@@ -89,23 +96,23 @@ function complexity(filePath)
 
                         builder.FunctionName = functionName(node);
 			
-			traverseWithParents(node, function(child_)
+                        traverseWithParents(node, function(child_)
                         {
                                 //The max length of a message chain in a function
                                 if(child_.type === 'MemberExpression')
                                 {
                                         var length = 1;
                                         //Iterate to check the number of MemberExpression in the tree
-					traverseWithParents(child_.object, function(_child_)
+                                        traverseWithParents(child_.object, function(_child_)
                                         {
                                                 if(_child_.type === 'MemberExpression')
                                                 {
                                                         length++;                                                                                                                                                   }
                                                 //Function to check and put the max of the length and MaxMessageChain as the final max value
-						builder.MaxMessageChain = Math.max(builder.MaxMessageChain,length);
-					});
-				}
-			});
+                                                builder.MaxMessageChain = Math.max(builder.MaxMessageChain,length);
+                                        });
+                                }
+                        });
                         if(builder.MaxMessageChain > threshold_message_chains)
                         {
                                 throw new Error("FileName : [" + filePath + "] FunctionName : [" + builder.FunctionName + "] exceeded the threshold value for maximum message chains having " + builder.MaxMessageChain + " message chains");
@@ -118,17 +125,31 @@ function complexity(filePath)
 
 // Helper function for counting children of node.
 function childrenLength(node)
-{                                                                                                                                                                           var key, child;
-        var count = 0;                                                                                                                                                      for (key in node)                                                                                                                                                   {                                                                                                                                                                           if (node.hasOwnProperty(key))                                                                                                                                       {                                                                                                                                                                           child = node[key];                                                                                                                                                  if (typeof child === 'object' && child !== null && key != 'parent')                                                                                                 {                                                                                                                                                                           count++;
+{
+        var key, child;
+        var count = 0;
+	for (key in node)
+	{
+		if (node.hasOwnProperty(key))
+		{
+			child = node[key];
+			if (typeof child === 'object' && child !== null && key != 'parent') 
+			{
+				count++;
                         }
                 }
-        }                                                                                                                                                                   return count;
+        }
+	return count;
 }
 
 // Helper function for printing out function name.
 function functionName( node )
-{                                                                                                                                                                           if( node.id )
-        {                                                                                                                                                                           return node.id.name;                                                                                                                                        }                                                                                                                                                                   return "anon function @" + node.loc.start.line;
+{ 
+	if( node.id )
+        {
+		return node.id.name;
+	}
+	return "anon function @" + node.loc.start.line;
 }
 
 
