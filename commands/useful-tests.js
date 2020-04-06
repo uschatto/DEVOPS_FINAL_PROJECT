5491b23a46775d598190d4a0350d2133bbe74be0
kmkothav <kmkothav@ncsu.edu>
2020-04-06 17:45:20 -0400

    Code cleaning useful-tests

--- commands/useful-tests.js
+++ commands/useful-tests.js
@@ -11,22 +11,6 @@ exports.command = 'useful-tests';
 exports.desc = 'Initiate analysis of test suite for iTrust to run `-c` numbers of times';
 exports.builder = yargs => {
     yargs.options({
-        file: {
-            describe: 'Provide the path to the main playbook.yml',
-            type: 'string',
-            default: 'pipeline/fuzzer.yml'
-        },
-        inventory: {
-            describe: 'Provide the path to the inventory file',
-            type: 'string',
-            default: 'pipeline/inventory.ini'
-        },
-        vaultpass: {
-            alias: 'vp',
-            describe: 'the password to use for ansible vault',
-            default: 'DEVOPS16',
-            type: 'string'
-        },
 	    count: {
             alias: 'c',
 	        describe: 'test suite for iTrust to run `-c` numbers of times.',
@@ -38,33 +22,16 @@ exports.builder = yargs => {
 
 
 exports.handler = async argv => {
-    const { file, inventory, vaultpass, count } = argv;
+    const { count } = argv;
     (async () => {
-
-        if (fs.existsSync(path.resolve(file)) && fs.existsSync(path.resolve(inventory))) 
-        {
-             await run( file, inventory, vaultpass, count );
-        }
-        else
-        {
-             console.error(`'File or inventory dont exist. Make sure to provide path from root of pipeline directory'`);  
-        }          
-
+        await run( count );
     })();
 
 };
 
-async function run(file, inventory, vaultpass, count) {
+async function run(count) {
 
-    // the paths should be from root of pipeline directory
-    // Transforming path of the files in host to the path in VM's shared folder
-    let filePath = '/bakerx/'+ file;
-    let inventoryPath = '/bakerx/' +inventory;
-
-    let vaultPath = '/tmp/.vault_pass';
-
-    console.log(chalk.blueBright('Running fuzzer.js for useful-tests...'));
-    //result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath} ${vaultPath} ${count}`, 'vagrant@192.168.33.11');
+    console.log(chalk.blueBright('Running fuzzer.js for useful-tests...'))
     result = sshSync(`node fuzzer.js ${count}`, 'vagrant@192.168.33.20');
     if( result.error ) { process.exit( result.status ); }
 }
