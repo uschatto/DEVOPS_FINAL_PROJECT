8d768d3afc7914c0638242ae656b58417eb1c7bb
kmkothav <kmkothav@ncsu.edu>
2020-04-17 19:18:16 -0400

    Direct ssh with root user

--- commands/prod.js
+++ commands/prod.js
@@ -42,22 +42,15 @@ exports.handler = async argv => {
 
 class DigitalOceanProvider{
 
-    async createVm (name, region, image){
+    async createVm (name, region, image, key_list){
 
-        let keyurl = "https://api.digitalocean.com/v2/account/keys";
-        let key_response = await got.post(keyurl,
-        {
-          headers: headers  
-        }
-        );
-        console.log("SSH Keys ",key_response);
         var data = 
         {
           "name": name,
           "region":region,
           "size":"512mb",
           "image":image,
-          "ssh_keys":keyid_list,
+          "ssh_keys":key_list,
           "backups":false,
           "ipv6":false,
           "user_data":null,
@@ -88,6 +81,22 @@ class DigitalOceanProvider{
         }
     }
 
+    async get_keys(){
+      let keyurl = "https://api.digitalocean.com/v2/account/keys";
+        let key_response = await got(keyurl,
+        {
+          headers: headers  
+        }
+        );
+        let key_list = []
+        let keyids_list = (JSON.parse(key_response.body))['ssh_keys'];
+        console.log("SSH Keys ",keyids_list);
+        keyids_list.forEach(element => {
+          key_list.push(element['id'])
+        });
+        return key_list
+    }
+
 }
 
 async function run() {
@@ -95,8 +104,9 @@ async function run() {
   var image = "ubuntu-18-04-x64";
   var region = "blr1";
   var names = ['monitor','blue','green'];
+  let key_list = await client.get_keys();
   for(var i = 0; i < names.length; i++){
-    await client.createVm(names[i],region,image);
+    await client.createVm(names[i], region, image, key_list);
   }
   console.log("Droplet Ids: ", ids)
 }
