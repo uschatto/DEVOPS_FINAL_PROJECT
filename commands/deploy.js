a5a3707d772e3fa1a246e12303191f31a97bbb23
kcshah <kcshah@ncsu.edu>
2020-04-26 00:51:36 -0400

    Merged build command with deploy

--- commands/deploy.js
+++ commands/deploy.js
@@ -5,6 +5,8 @@ const os = require('os');
 const fs = require('fs');
 
 require('dotenv').config()
+let url = "http://" + process.env.JENKINS_USER + ":" + process.env.JENKINS_PASS + "@" + process.env.JENKINS_URL
+const jenkins = require('jenkins')({ baseUrl : url, crumbIssuer: true, promisify: true });
 
 const sshSync = require('../lib/ssh');
 
@@ -40,6 +42,8 @@ exports.builder = yargs => {
 exports.handler = async argv => {
     const { app, itrustFile, checkboxFile, inventory, vaultpass } = argv;
     (async () => {
+        // await createWar();
+        await preDeploy();
         await run(app, itrustFile, checkboxFile, inventory, vaultpass);
     })();
 };
@@ -71,3 +75,67 @@ async function run(app, itrustFile, checkboxFile, inventory, vaultpass) {
     if( result.error ) { process.exit( result.status ); }
 
 }
+
+async function waitOnQueue(id) {
+    return new Promise(function(resolve,reject)
+    {
+          jenkins.queue.item(id, function(err,item) {
+		if(err) throw err;
+		if(item.executable) {
+			console.log("number:",item.executable.number);
+			resolve(item.executable.number);
+		}
+		else if (item.cancelled)
+		{
+			console.log("cancelled");
+			reject("cancelled");
+		}
+		else {
+			setTimeout(async function() {
+				resolve(await waitOnQueue(id));
+			}, 5000);
+		}
+          });
+
+    });
+}
+
+async function triggerBuild(job) {
+    let queueId = await jenkins.job.build(job);
+    let buildId = await waitOnQueue(queueId);
+    return buildId;
+}
+
+async function preDeploy(){
+    let buildId = await triggerBuild(process.env.JENKINS_JOB_ITRUST).catch( e => console.log(e));
+    
+    console.log(chalk.blueBright(`Build number :  ${buildId}`));
+
+    console.log(chalk.green(`Build output`));
+    var log = jenkins.build.logStream(process.env.JENKINS_JOB_ITRUST, buildId);
+ 
+    log.on('data', function(text) {
+      process.stdout.write(text);
+    });
+ 
+    log.on('error', function(err) {
+      console.log('error', err);
+    });
+ 
+    log.on('end', function() {
+      console.log('end');
+      copyWarFile();
+    });
+}
+
+function copyWarFile() {
+    result = sshSync(`scp -i ~/.ssh/mm_rsa vagrant@192.168.33.20:${process.env.JENKINS_WAR_PATH} ${process.env.ITRUST_WAR_PATH}`,'vagrant@192.168.33.11');       
+    if( result.error ) { process.exit( result.status ); }
+    else{ console.log(chalk.green(("Successfully copied iTrust war file"))); }
+}
+
+// async function createWar(){
+//     result = sshSync(`cd /var/lib/jenkins/workspace/iTrust/iTrust2 && mvn package`,'vagrant@192.168.33.20');     
+//     if( result.error ) { process.exit( result.status ); }
+//     else{ console.log(chalk.green(("Successfully copied iTrust war file"))); }
+// }
--- commands/setup.js
+++ commands/setup.js
@@ -7,10 +7,6 @@ const fs = require('fs');
 const scpSync = require('../lib/scp');
 const sshSync = require('../lib/ssh');
 
-require('dotenv').config()
-let url = "http://" + process.env.JENKINS_USER + ":" + process.env.JENKINS_PASS + "@" + process.env.JENKINS_URL
-const jenkins = require('jenkins')({ baseUrl : url, crumbIssuer: true, promisify: true });
-
 exports.command = 'setup';
 exports.desc = 'Provision and configure the configuration server';
 exports.builder = yargs => {
@@ -58,7 +54,6 @@ exports.handler = async argv => {
         if (fs.existsSync(path.resolve(file)) && fs.existsSync(path.resolve(inventory))) 
         {
              await run( privateKey, file, inventory, vaultpass, ghUser, ghPass );
-             await postRun();
         }
         else
         {
@@ -120,62 +115,4 @@ async function run(privateKey, file, inventory, vaultpass, ghUser, ghPass) {
     console.log(chalk.blueBright('Running ansible script...'));
     result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath} ${vaultPath} ${ghUser} ${ghPass}`, 'vagrant@192.168.33.11');
     if( result.error ) { process.exit( result.status ); }
-}
-
-async function waitOnQueue(id) {
-    return new Promise(function(resolve,reject)
-    {
-          jenkins.queue.item(id, function(err,item) {
-		if(err) throw err;
-		if(item.executable) {
-			console.log("number:",item.executable.number);
-			resolve(item.executable.number);
-		}
-		else if (item.cancelled)
-		{
-			console.log("cancelled");
-			reject("cancelled");
-		}
-		else {
-			setTimeout(async function() {
-				resolve(await waitOnQueue(id));
-			}, 5000);
-		}
-          });
-
-    });
-}
-
-async function triggerBuild(job) {
-    let queueId = await jenkins.job.build(job);
-    let buildId = await waitOnQueue(queueId);
-    return buildId;
-}
-
-async function postRun(){
-    let buildId = await triggerBuild(process.env.JENKINS_JOB_ITRUST).catch( e => console.log(e));
-    
-    console.log(chalk.blueBright(`Build number :  ${buildId}`));
-
-    console.log(chalk.green(`Build output`));
-    var log = jenkins.build.logStream(process.env.JENKINS_JOB_ITRUST, buildId);
- 
-    log.on('data', function(text) {
-      process.stdout.write(text);
-    });
- 
-    log.on('error', function(err) {
-      console.log('error', err);
-    });
- 
-    log.on('end', function() {
-      console.log('end');
-      copyWarFile();
-    });
-}
-
-function copyWarFile() {
-    result = sshSync(`scp -i ~/.ssh/mm_rsa vagrant@192.168.33.20:${process.env.JENKINS_WAR_PATH} ${process.env.ITRUST_WAR_PATH}`,'vagrant@192.168.33.11');       
-    if( result.error ) { process.exit( result.status ); }
-    else{ console.log(chalk.green(("Successfully copied iTrust war file"))); }
 }
